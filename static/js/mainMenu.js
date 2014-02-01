636d7c3d8663e7b849e44718e2b27e9be805f277
cbrem <connorbrem@gmail.com>
2014-02-01 12:16:08 -0500

    added mainMenu.js

---
+++ static/js/mainMenu.js
@@ -0,0 +1,24 @@
+/*
+ * Creates a new comic, and navigates to an edit view for that comic's first
+ * panel.
+ */
+function newComic() {
+    var situation = document.getElementById("situation-entry").value;
+
+    // Require situation to be non-empty.
+    if (!situation) {
+        alert("Enter a situation!");
+        return;
+    }
+
+    var data = {
+        situation: situation
+    };
+    $.ajax({
+        type: 'POST',
+        url: '/',
+        data: data
+    }).done(function(comID) {
+        window.location = '/edit?comID=' + comID;
+    });
+}
