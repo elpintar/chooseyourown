dc54cc1efd6ab292e9d59d754a0c05bead7e27dd
cbrem <connorbrem@gmail.com>
2014-02-01 03:39:17 -0500

    started da editing

--- static/js/WrappedSketch.js
+++ static/js/WrappedSketch.js
@@ -18,16 +18,16 @@ WrappedSketch.mimeType = 'image/png';
 /* Sends a data URL based on a snapshot of this WrappedSketch to the server. */
 WrappedSketch.prototype.sendPanelData = function() {
     var url = document.URL;
-    var prevId = url.slice(url.lastIndexOf('/') + 1);
-    var image = this.$canvas[0].toDataURL(WrappedSketch.mimeType);
+    var prevID = url.slice(url.lastIndexOf('/') + 1);
+    var img = this.$canvas[0].toDataURL(WrappedSketch.mimeType);
     var whatsHappening = this.$desc[0].value;
     var data = {
-        image: image,
+        img: img,
         whatsHappening: whatsHappening
     };
     $.ajax({
         type: 'POST',
-        url: "/edit/" + prevId,
+        url: "/edit/" + prevID,
         data: data
     }).done(function(id) {
         window.location.href = "/read/" + id;
