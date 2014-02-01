00956a535b1a91522c2e2b051ec022c50469bd08
Spencer Barton <sbarton272@gmail.com>
2014-02-01 11:34:31 -0500

    Added delete methods for the dataBase (YIKES)

--- DataBase.py
+++ DataBase.py
@@ -7,83 +7,161 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 
 class DataBase(object):
-	"""DatBase instantuates a conncetion to our dataBase and performs queries"""
+    """DatBase instantiates a connection to our dataBase and performs queries"""
 
-	def __init__(self):
-		# Database names
-		dbName = "heroku_app21800319"
-		comicsCollectionName = "Comics"
-		panelsCollectionName = "Panels"
-		self.idField = "_id"
-		self.nextIDField = "nextIDs"
+    def __init__(self):
+        # Database names
+        dbName = "heroku_app21800319"
+        comicsCollectionName = "Comics"
+        panelsCollectionName = "Panels"
+        self.idField = "_id"
+        self.nextIDField = "nextIDs"
+        self.firstPanelIDField = "startingPanelID"
 
-		# load settings
-		settings = {}
-		execfile("dataBaseSettings.conf", settings)
-		
-		client = MongoClient( settings["URI"] )
-		
-		# collections
-		db = client[dbName]
-		self.comicsCollection = db[comicsCollectionName]
-		self.panelsCollection = db[panelsCollectionName]
+        # load settings
+        settings = {}
+        execfile("dataBaseSettings.conf", settings)
+        
+        client = MongoClient( settings["URI"] )
+        
+        # collections
+        db = client[dbName]
+        self.comicsCollection = db[comicsCollectionName]
+        self.panelsCollection = db[panelsCollectionName]
 
-	#==============================================
-	# GET Methods
-	#==============================================
+    #==============================================
+    # GET Methods
+    #==============================================
 
-	def getPanelByID(self, _id):
-		"""Get panal by ID, return dict"""
-		return self.panelsCollection.find_one({self.idField : ObjectId(_id)})
+    def getPanelByID(self, _id):
+        """Get panal by ID, return dict and None on error"""
+        if not ObjectId.is_valid(_id):
+            return None
+        return self.panelsCollection.find_one({self.idField : ObjectId(_id)})
 
-	def getComicByID(self, _id):
-		"""Get comic by ID, return dict"""
-		return self.comicsCollection.find_one({self.idField : ObjectId(_id)})
+    def getComicByID(self, _id):
+        """Get comic by ID, return dict and None on error"""
+        if not ObjectId.is_valid(_id):
+            return None
+        return self.comicsCollection.find_one({self.idField : ObjectId(_id)})
 
-	def getAllComics(self):
-		"""Get all comics and return in list ordered by ID"""
-		cur = self.comicsCollection.find()
-		return list(cur.sort(self.idField))
+    def getAllComics(self):
+        """Get all comics and return in list ordered by ID or None on error"""
+        if not ObjectId.is_valid(_id):
+            return None
+        cur = self.comicsCollection.find()
+        return list(cur.sort(self.idField))
 
-	#==============================================
-	# NEW Methods
-	#==============================================
+    #==============================================
+    # NEW Methods
+    #==============================================
 
-	def newComic(self, situation, startingPanelID):
-		"""create a new comic given the arguments,
-			generates a unique _id. Returns _id"""
-		return self.comicsCollection.insert( { "situation": situation,
-									   "startingPanelID" : startingPanelID })
+    def newComic(self, situation):
+        """create a new comic given the arguments,
+            generates a unique _id. Returns _id"""
+        return self.comicsCollection.insert( { "situation": situation,
+                                       "startingPanelID" : None })
 
-	def newPanel(self, prevID, whatIsHappening, img):
-		"""create a new panal given the arguments,
-			generates a unique _id. Returns _id.
-			Also updates the prevID to point to the 
-			new ID"""
-		newID = self.panelsCollection.insert( { "prevID": prevID,
-									   "whatIsHappening" : whatIsHappening,
-									   "nextIDs" : [],
-									   "img" : img })
-		# update prev to point to new
-		self._updateNextIDs(prevID, newID)
-		return newID
+    def newPanel(self, prevID, whatIsHappening, img):
+        """create a new panal given the arguments,
+            generates a unique _id. Returns _id and None on error.
+            Also updates the prevID to point to the 
+            new ID"""
+        if not ObjectId.is_valid(prevID):
+            return None
 
-	#==============================================
-	# UPDATE Methods
-	#==============================================
+        newID = self.panelsCollection.insert( { "prevID": prevID,
+                                       "whatIsHappening" : whatIsHappening,
+                                       "nextIDs" : [],
+                                       "img" : img })
+        # update prev to point to new
+        if (self._updateNextIDs(prevID, newID) == True):
+            return newID
+        return None
 
-	def _updateNextIDs(self, frameID, nextFrameID):
-		"""This is a private method.
-		   Add the nextID to the given frame id,
-		   return true if successful, false otherwise"""
-		frameDoc = self.panelsCollection.find_one( {self.idField : ObjectId(frameID)} )
-		if frameDoc == None:
-			return False
+    def newFirstPanel(self, comicID, whatIsHappening, img):
+        """create a new panal given the arguments,
+            generates a unique _id. Returns _id and None on error.
+            Also updates the comic to point to the 
+            new panel ID"""
+        if not ObjectId.is_valid(comicID):
+            return None
 
-		nextIDs = frameDoc["nextIDs"]
-		nextIDs.append(nextFrameID)
-		nextIDs = list(set(nextIDs)) # only unique next panels
+        # prevID is not present as this is first panel in comic
+        newID = self.panelsCollection.insert( { "prevID": None,
+                                       "whatIsHappening" : whatIsHappening,
+                                       "nextIDs" : [],
+                                       "img" : img })
+        # update comic to point to newID as the startingPanelID
+        if (self._updateStartingPanelID(comicID, newID) == True):
+            return newID
+        return None # error in updating
 
-		self.panelsCollection.update( { self.idField : ObjectId(frameID)}, 
-									  { "$set" : {self.nextIDField : nextIDs} })
-		return True
+
+    #==============================================
+    # UPDATE Methods
+    #==============================================
+
+    def _updateNextIDs(self, frameID, nextFrameID):
+        """This is a private method.
+           Add the nextID to the given frame id,
+           return true if successful, false otherwise"""
+        if (not ObjectId.is_valid(frameID)) or (not ObjectId.is_valid(frameID)):
+            return False
+
+        frameDoc = self.panelsCollection.find_one( {self.idField : 
+                                                        ObjectId(frameID)} )
+        if frameDoc == None:
+            return False
+
+        nextIDs = frameDoc["nextIDs"]
+        nextIDs.append(nextFrameID)
+        nextIDs = list(set(nextIDs)) # only unique next panels
+
+        if( self.panelsCollection.update( { self.idField : ObjectId(frameID)}, 
+                        { "$set" : {self.nextIDField : nextIDs} }) != None):
+            return True
+        return False # update unsuccessful 
+
+    def _updateStartingPanelID(self, comicID, firstPanelID):
+        """This is a private method.
+           Add the firstPanelID to the given comic as the starting panel,
+           return true if successful, false otherwise"""
+        if (not ObjectId.is_valid(comicID)) or (not ObjectId.is_valid(firstPanelID)):
+            return False
+
+        if (self.comicsCollection.find_and_modify({self.idField: ObjectId(comicID)}, 
+                {"$set" : {self.firstPanelIDField : firstPanelID} }) != None):
+            return True
+        return False # update unsuccessful
+
+#$%&&^#$&*^#$%^@&Q*$*$#^%&&^$^^%&^#$^#%%&^#@%*&$#(*@#(*$*))
+# WARNING WARNING YOU ARE ENTERING THE SCARY ZONE
+# ENTER AT YOUR OWN HAZARD -- YOU MAY NOT RETURN
+#$%&&^#$&*^#$%^@&Q*$*$#^%&&^$^^%&^#$^#%%&^#@%*&$#(*@#(*$*))
+
+    #==============================================
+    # DELETE Methods
+    #==============================================
+
+    def deleteComic(self, _id):
+        """Delete comic with given ID. 
+           Return True on success, false otherwise"""
+        if not ObjectId.is_valid(_id):
+            return False
+        try:
+            self.comicsCollection.remove({self.idField : ObjectId(_id)})
+            return True
+        except  pymongo.errors.OperationFailure as E:
+            return False
+
+    def deletePanel(self, _id):
+        """Delete panel with given ID. 
+           Return True on success, false otherwise"""
+        if not ObjectId.is_valid(_id):
+            return False
+        try:
+            self.panelsCollection.remove({self.idField : ObjectId(_id)})
+            return True
+        except  pymongo.errors.OperationFailure as E:
+            return False
