3952252204941a140179c3d6f278f6738a144721
billy <wtwood@andrew.cmu.edu>
2014-02-01 13:18:22 -0500

    fixed spacing, handled lack of queries

--- chooseYourOwn.py
+++ chooseYourOwn.py
@@ -17,10 +17,10 @@ ERROR missing query - redirect home
 
 import os
 import random
-from urllib import quote
+from urllib   import quote
 from DataBase import DataBase
-from bottle import route, run, template, post, get, delete, static_file, \
-                   request, response
+from bottle   import route, run, template, post, get, delete, static_file, \
+                     request, response
 
 #=============================================
 # Globals
@@ -36,7 +36,7 @@ def comBut(situation, panelID):
     # Return the button containing described with text, situation,
     # and links to /read/panelID
     return ('<a class="choice" href=\"'+ '/read/?panelID=' +
-        str(panelID) + '\">' + situation + '</a>' + '\n')
+            str(panelID) + '\">' + situation + '</a>' + '\n')
 
 #=============================================
 # Home /
@@ -54,19 +54,6 @@ def displayMenu():
 
 @post("/")
 def createComic():
-<<<<<<< HEAD
-	situation = request.params['situation']
-	comicID = db.newComic(situation)
-	response.headers['Context-Type'] = 'text/plain'
-	return comicID
-
-@delete("/")
-def deleteComic():
-	comID = request.params['comID']
-	confirm = db.deleteComic(comID)
-	response.headers['Context-Type'] = 'text/plain'
-	return confirm
-=======
     # TODO
     situation = request.params['situation']
     id = db.newComic(situation)
@@ -82,77 +69,9 @@ def deleteComic():
 #=============================================
 # Edit /<comic>/edit
 #=============================================
->>>>>>> 62482eaec52fce07783b2175b32d77b6715cc6ac
 
 @route("/edit")
 def displayEdit(comic):
-<<<<<<< HEAD
-	# Returns the edit page
-	prevID = request.query.prevID
-	prevLink = "/" + comic + "/read?panelID=" + prevID
-	return template('edit_template', prevLink=prevLink
-
-@post("/<comic>/edit")
-def display_edit(comic):
-	# Adds a new panel to the database and returns its ID.
-	prevID = request.params['prevID']
-	comID = request.params['comID']
-	whatsHappening = request.params['whatsHappening']
-	img = request.params['img']
-	if prevID != '':
-		newID = newPanel(prevID, whatsHappening, img)
-	else:
-		newID = db.newFirstPanel(comID, whatsHappening, img)
-	response.headers['Context-Type'] = 'text/plain'
-	return newID
-
-@route("/<comic>/read")
-def displayPanel(comic):
-	# Returns the display screen for the given panel
-	panel = request.query.panelID
-	pan = db.getPanel(panel)
-	img = pan['img']
-	par = pan['prevID']
-	children = pan['nextIDs']
-	if len(children) == 0:
-		nextLink = '/' + comic + '/edit?prevID=' + panel
-	elif len(children) == 1:
-		nextLink = '/' + comic + '/read?panelID=' + children[0]
-	else:
-		nextLink = '/' + comic + '/choose?prevID=' + panel
-	prevLink = '/' + comic + '/read?panelID=' + par
-	return template('read_template', nextLink=nextLink,
-			prevLink=prevLink, img=img)
-
-@route("/<comic>/choose")
-def displayNext(comic):
-	# Returns the next-panel decision screen
-	panel = request.query.panelID
-	pan = db.getPanel(panel)
-	par = pan['prevID']
-	all_children = pan['nextIDs']
-	child = [db.getPanel(ch_id) for ch_id in child_ids]
-	comicID = ''
-	comList = ''
-	newComicButton = ('<a class="choice" href=\"' + comicName +
-			  '/edit?prevID=' + panelID + 
-			  '&comicID' + comicID + '\">')
-	for ch in child:
-		comList = strng + comBut(ch['situation'],
-					 ch['startingPanelID'])
-		comList = strgn + '\n'
-	if len(child) == 0:
-		questionText = 'The End'
-		newComicButton += 'Continue?'
-	else:
-		questionText = 'What happens next?'
-		newComicButton += 'Or something else...'
-	newComicButton += '</a>'
-	
-	return template('choose_template', panel=panel, parent=par,
-			comicList=comList, questText=questionText,
-			newComicText=newComicText)
-=======
     # Returns the edit page
     # TODO add logic for first or continuation
     return template('edit_template')
@@ -178,18 +97,22 @@ def postEdit(comic):
 @route("/read")
 def displayPanel(comic):
     # Returns the display screen for the given panel
-    panel = request.query.panelID
-    pan = db.getPanelByID(panel)
-    img = pan['img']
-    par = pan['prevID']
-    children = pan['nextIDs']
-    if len(children) == 0:
-        nextLink = '/edit?prevID=' + panel
-    elif len(children) == 1:
-        nextLink = '/read?panelID=' + children[0]
+    try:
+        panel = request.query.panelID
+    except:
+        return displayMenu()
     else:
-        nextLink = '/choose?prevID=' + panel
-    return template('read_template', nextLink=nextLink, parent=par, img=img)
+        pan = db.getPanelByID(panel)
+        img = pan['img']
+        par = pan['prevID']
+        children = pan['nextIDs']
+        if len(children) == 0:
+            nextLink = '/edit?prevID=' + panel
+        elif len(children) == 1:
+            nextLink = '/read?panelID=' + children[0]
+        else:
+            nextLink = '/choose?prevID=' + panel
+        return template('read_template', nextLink=nextLink, parent=par, img=img)
 
 #=============================================
 # Choose next panel /<comic>/choose
@@ -198,36 +121,37 @@ def displayPanel(comic):
 @route("/choose")
 def displayNext(comic):
     # Returns the next-panel decision screen
-    panel = request.query.panelID
-    pan = db.getPanel(panel)
-    par = pan['prevID']
-    all_children = pan['nextIDs']
-    child = [db.getPanel(ch_id) for ch_id in child_ids]
-    comicID = ''
-    comList = ''
-    newComicButton = ('<a class="choice" href=\"' +
-              '/edit?prevID=' + panelID + 
-              '&comicID' + comicID + '\">')
-    for ch in child:
-        comList = comList + comBut(ch['situation'],
-                     ch['startingPanelID'])
-    if len(child) == 0:
-        questionText = 'The End'
-        newComicButton += 'Continue?'
+    try:
+        panel = request.query.panelID
+    except:
+        return displayMenu()
     else:
-        questionText = 'What happens next?'
-        newComicButton += 'Or something else...'
-    newComicButton += '</a>'
+        pan = db.getPanel(panel)
+        par = pan['prevID']
+        all_children = pan['nextIDs']
+        child = [db.getPanel(ch_id) for ch_id in child_ids]
+        comicID = ''
+        comList = ''
+        newComicButton = ('<a class="choice" href=\"' +
+                         '/edit?prevID=' + panelID + 
+                         '&comicID' + comicID + '\">')
+        for ch in child:
+            comList = comList + comBut(ch['situation'], ch['startingPanelID'])
+        if len(child) == 0:
+            questionText = 'The End'
+            newComicButton += 'Continue?'
+        else:
+            questionText = 'What happens next?'
+            newComicButton += 'Or something else...'
+        newComicButton += '</a>'
     
-    return template('choose_template', panel=panel, parent=par,
-            comicList=comList, questText=questionText,
-            newComicText=newComicText)
-            
+        return template('choose_template', panel=panel, parent=par,
+                        comicList=comList, questText=questionText,
+                        newComicText=newComicText)
 
 #=============================================
 # Static files
 #=============================================
->>>>>>> 62482eaec52fce07783b2175b32d77b6715cc6ac
 
 # What follows is copied from the Internet.
 # Thank you Internet.
