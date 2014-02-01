d236ec44c05167027252b139b02f6695eff24f03
Spencer Barton <sbarton272@gmail.com>
2014-02-01 03:26:10 -0500

    DataBase.py all up and happy

--- DataBase.py
+++ DataBase.py
@@ -22,25 +22,22 @@ class DataBase(object):
 		
 		client = MongoClient( settings["URI"] )
 		
-		# databases
+		# collections
 		db = client[dbName]
 		self.comicsCollection = db[comicsCollectionName]
 		self.panelsCollection = db[panelsCollectionName]
-
-		print self.newComic("boo", "THIS COMIC IS AWESOME")
-		self.newPanel("52ec9367e4b088d0cdd3c1f2", "No worky worky -> derp", "IMG OMG SO PERTY")
 
 	#==============================================
 	# GET Methods
 	#==============================================
 
-	def getPanalByID(self, _id):
+	def getPanelByID(self, _id):
 		"""Get panal by ID, return dict"""
-		return self.panelsCollection.find_one({self.idField : ObjectID(_id)})
+		return self.panelsCollection.find_one({self.idField : ObjectId(_id)})
 
 	def getComicByID(self, _id):
 		"""Get comic by ID, return dict"""
-		return self.comicsCollection.find_one({self.idField : ObjectID(_id)})
+		return self.comicsCollection.find_one({self.idField : ObjectId(_id)})
 
 	def getAllComics(self):
 		"""Get all comics and return in list ordered by ID"""
@@ -62,11 +59,10 @@ class DataBase(object):
 			generates a unique _id. Returns _id.
 			Also updates the prevID to point to the 
 			new ID"""
-		newID = self.comicsCollection.insert( { "prevID": prevID,
+		newID = self.panelsCollection.insert( { "prevID": prevID,
 									   "whatIsHappening" : whatIsHappening,
 									   "nextIDs" : [],
 									   "img" : img })
-		print newID
 		# update prev to point to new
 		self.updateNextIDs(prevID, newID)
 		return newID
@@ -78,7 +74,7 @@ class DataBase(object):
 	def updateNextIDs(self, frameID, nextFrameID):
 		"""Add the nextID to the given frame id,
 			return true if successful, false otherwise"""
-		frameDoc = self.panelsCollection.find_one( {self.idField : frameID} )
+		frameDoc = self.panelsCollection.find_one( {self.idField : ObjectId(frameID)} )
 		if frameDoc == None:
 			return False
 
@@ -86,7 +82,7 @@ class DataBase(object):
 		nextIDs.append(nextFrameID)
 		nextIDs = list(set(nextIDs)) # only unique next panels
 
-		self.panelsCollection.update( { self.idField : ObjectID(frameID)}, 
+		self.panelsCollection.update( { self.idField : ObjectId(frameID)}, 
 									  { "$set" : {self.nextIDField : nextIDs} })
 		return True
 
