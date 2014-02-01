b4db30331c0b86c4d4a880f582d0439a37dead1d
Spencer Barton <sbarton272@gmail.com>
2014-02-01 03:05:46 -0500

    Database stuff working except newPanel

--- DataBase.py
+++ DataBase.py
@@ -4,6 +4,7 @@
 
 import json
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 class DataBase(object):
 	
@@ -11,8 +12,9 @@ class DataBase(object):
 		# Database names
 		dbName = "heroku_app21800319"
 		comicsCollectionName = "Comics"
-		panalsCollectionName = "Panals"
-		self.idField = "id"
+		panelsCollectionName = "Panels"
+		self.idField = "_id"
+		self.nextIDField = "nextIDs"
 
 		# load settings
 		settings = {}
@@ -22,44 +24,70 @@ class DataBase(object):
 		
 		# databases
 		db = client[dbName]
-		self.comicsDB = db[comicsCollectionName]
-		self.panalsDB = db[panalsCollectionName]
+		self.comicsCollection = db[comicsCollectionName]
+		self.panelsCollection = db[panelsCollectionName]
 
-		print self.getPanalByID(3)
-		print self.getComicByID(1)
-		print self.getAllComics()
+		print self.newComic("boo", "THIS COMIC IS AWESOME")
+		self.newPanel("52ec9367e4b088d0cdd3c1f2", "No worky worky -> derp", "IMG OMG SO PERTY")
 
 	#==============================================
 	# GET Methods
 	#==============================================
 
-	def getPanalByID(self, id):
+	def getPanalByID(self, _id):
 		"""Get panal by ID, return dict"""
-		return self.panalsDB.find_one({self.idField:id})
+		return self.panelsCollection.find_one({self.idField : ObjectID(_id)})
 
-	def getComicByID(self, id):
+	def getComicByID(self, _id):
 		"""Get comic by ID, return dict"""
-		return self.comicsDB.find_one({self.idField:id})
+		return self.comicsCollection.find_one({self.idField : ObjectID(_id)})
 
 	def getAllComics(self):
 		"""Get all comics and return in list ordered by ID"""
-		cur = self.comicsDB.find()
+		cur = self.comicsCollection.find()
 		return list(cur.sort(self.idField))
 
 	#==============================================
 	# NEW Methods
 	#==============================================
 
-	def newComic(self):
-		return 42
+	def newComic(self, situation, startingPanelID):
+		"""create a new comic given the arguments,
+			generates a unique _id. Returns _id"""
+		return self.comicsCollection.insert( { "situation": situation,
+									   "startingPanelID" : startingPanelID })
 
-	def newPanal(self):
-		return 42
+	def newPanel(self, prevID, whatIsHappening, img):
+		"""create a new panal given the arguments,
+			generates a unique _id. Returns _id.
+			Also updates the prevID to point to the 
+			new ID"""
+		newID = self.comicsCollection.insert( { "prevID": prevID,
+									   "whatIsHappening" : whatIsHappening,
+									   "nextIDs" : [],
+									   "img" : img })
+		print newID
+		# update prev to point to new
+		self.updateNextIDs(prevID, newID)
+		return newID
 
 	#==============================================
 	# UPDATE Methods
 	#==============================================
 
-	
+	def updateNextIDs(self, frameID, nextFrameID):
+		"""Add the nextID to the given frame id,
+			return true if successful, false otherwise"""
+		frameDoc = self.panelsCollection.find_one( {self.idField : frameID} )
+		if frameDoc == None:
+			return False
+
+		nextIDs = frameDoc["nextIDs"]
+		nextIDs.append(nextFrameID)
+		nextIDs = list(set(nextIDs)) # only unique next panels
+
+		self.panelsCollection.update( { self.idField : ObjectID(frameID)}, 
+									  { "$set" : {self.nextIDField : nextIDs} })
+		return True
 
 DataBase()
